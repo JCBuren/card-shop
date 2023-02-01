31b8c30db9dac8f2cb2722c8e71a9cd34cdc0224
Jordan <JBuren1957@Gmail.com>
2023-02-01 10:53:40 -0600

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { AiOutlineMenu } from "react-icons/ai"
 
 const Navbar = () => {
 	return (
-		<div>
+		<div className="max-w-[1640px] mx-auto flex justify-between">
 			{/* Left Side */}
-			<div>
-				<div>
+			<div className="flex items-center">
+				<div className="cursor-pointer">
 					<AiOutlineMenu size={35} />
 				</div>
-				<h1>
+				<h1 className="text-2xl sm:text-3xl lg:text-4xl">
 					Hello <span>World</span>
 				</h1>
 			</div>
